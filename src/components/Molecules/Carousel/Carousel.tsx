e559ef463ffcc485b7e97a3cce5a8e1aeff831a8
ashanpw <ashan399@gmail.com>
2024-07-08 18:16:07 -0700

    feat: add srcset to project video and img to reduce payload sizes

--- src/components/Molecules/Carousel/Carousel.tsx
+++ src/components/Molecules/Carousel/Carousel.tsx
@@ -1,9 +1,9 @@
 import useEmblaCarousel from "embla-carousel-react";
 import {
-  EmblaContainer,
-  EmblaDiv,
-  EmblaSlide,
-  EmblaViewport,
+    EmblaContainer,
+    EmblaDiv,
+    EmblaSlide,
+    EmblaViewport,
 } from "./Carousel.styles";
 
 import { useState } from "react";
@@ -12,60 +12,112 @@ import { motion } from "framer-motion";
 import { SourceAnimationVariants } from "../../../utils/Constants";
 
 export const Carousel = (props: any) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+    const [emblaRef, emblaApi] = useEmblaCarousel();
 
-  // Sets the selected index
-  const [selectedIdx, setSelectedIdx] = useState(0);
-  emblaApi?.on("select", () => setSelectedIdx(emblaApi?.selectedScrollSnap));
+    // Sets the selected index
+    const [selectedIdx, setSelectedIdx] = useState(0);
+    emblaApi?.on("select", () => setSelectedIdx(emblaApi?.selectedScrollSnap));
 
-  const scrollToSlide = (idx: number): void => {
-    emblaApi?.scrollTo(idx);
-  };
+    const scrollToSlide = (idx: number): void => {
+        emblaApi?.scrollTo(idx);
+    };
 
-  const gg = props.mediaList.map((media: any, idx: number) => {
+    const gg = props.mediaList.map((media: any, idx: number) => {
+        const imgSrcSet = [
+            `${media.src}-large.webp 1920w`,
+            `${media.src}-medium.webp 1280w`,
+            `${media.src}-small.webp 768w`,
+        ];
+        const imgFallBackSrc = `${media.src}.jpg`;
+        const videoSrcSet = [
+            `${media.src}-large.webm 1920w`,
+            `${media.src}-medium.webm 1280w`,
+            `${media.src}-small.webm 768w`,
+        ];
+        const videoFallBackSrc = `${media.src}.mp4`;
+
+        return (
+            <EmblaSlide key={idx}>
+                {media.type === "video" && (
+                    <video
+                        autoPlay={true}
+                        controls={false}
+                        loop={true}
+                        muted={true}
+                        width="100%"
+                        height="100%"
+                    >
+                        <motion.source
+                            src={videoSrcSet.join(", ")}
+                            style={{ aspectRatio: "16/9" }}
+                            initial="initial"
+                            whileInView="animate"
+                            variants={SourceAnimationVariants}
+                        />
+                        <motion.source
+                            src={videoFallBackSrc}
+                            style={{ aspectRatio: "16/9" }}
+                            initial="initial"
+                            whileInView="animate"
+                            variants={SourceAnimationVariants}
+                        />
+                        <motion.video
+                            src={videoFallBackSrc}
+                            style={{ aspectRatio: "16/9" }}
+                            initial="initial"
+                            whileInView="animate"
+                            variants={SourceAnimationVariants}
+                        />
+                    </video>
+                )}
+
+                {media.type === "image" && (
+                    <picture>
+                        <motion.source
+                            srcSet={imgSrcSet.join(", ")}
+                            style={{ aspectRatio: "16/9" }}
+                            width="100%"
+                            height="100%"
+                            variants={SourceAnimationVariants}
+                            initial="initial"
+                            whileInView="animate"
+                        />
+                        <motion.source
+                            srcSet={`${imgFallBackSrc} 1920w`}
+                            style={{ aspectRatio: "16/9" }}
+                            width="100%"
+                            height="100%"
+                            variants={SourceAnimationVariants}
+                            initial="initial"
+                            whileInView="animate"
+                        />
+                        <motion.img
+                            src={`${imgFallBackSrc}`}
+                            alt="A rad wolf"
+                            style={{ aspectRatio: "16/9" }}
+                            width="100%"
+                            height="100%"
+                            variants={SourceAnimationVariants}
+                            initial="initial"
+                            whileInView="animate"
+                        />
+                    </picture>
+                )}
+            </EmblaSlide>
+        );
+    });
+
     return (
-      <EmblaSlide key={idx}>
-        {media.type === "video" && (
-          <motion.video
-            src={media.src}
-            width="100%"
-            height="100%"
-            autoPlay={true}
-            controls={false}
-            loop={true}
-            muted={true}
-            initial="initial"
-            whileInView="animate"
-            variants={SourceAnimationVariants}
-          />
-        )}
+        <EmblaDiv>
+            <EmblaViewport ref={emblaRef}>
+                <EmblaContainer isMobile={props.isMobile}>{gg}</EmblaContainer>
+            </EmblaViewport>
 
-        {media.type === "image" && (
-          <motion.img
-            src={media.src}
-            style={{ aspectRatio: "16/9" }}
-            width="100%"
-            height="100%"
-            variants={SourceAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-          />
-        )}
-      </EmblaSlide>
+            <Dots
+                selectedIdx={selectedIdx}
+                size={props.mediaList.length}
+                onClickFn={scrollToSlide}
+            />
+        </EmblaDiv>
     );
-  });
-
-  return (
-    <EmblaDiv>
-      <EmblaViewport ref={emblaRef}>
-        <EmblaContainer isMobile={props.isMobile}>{gg}</EmblaContainer>
-      </EmblaViewport>
-
-      <Dots
-        selectedIdx={selectedIdx}
-        size={props.mediaList.length}
-        onClickFn={scrollToSlide}
-      />
-    </EmblaDiv>
-  );
 };
--- src/components/Molecules/Projects/Projects.tsx
+++ src/components/Molecules/Projects/Projects.tsx
@@ -3,171 +3,189 @@ import { IndentedContainer } from "../../Atoms/IndentedContainer/IndentedContain
 import { Carousel } from "../Carousel/Carousel";
 import { S } from "./Projects.styles";
 import {
-  LineAnimationVariants,
-  ListAnimationVariants,
+    AssetBucketUrlPrefix,
+    LineAnimationVariants,
+    ListAnimationVariants,
 } from "../../../utils/Constants";
 import { ProjectsHeader } from "../ProjectsHeader/ProjectsHeader";
 
 export const Projects = () => {
-  const text = {
-    title: "PROJECTS",
-    contentItems: [
-      {
-        name: "PROJECT WIND",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-wind/projectWindMain.mp4",
-            type: "video",
-          },
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-wind/projectWindAbout.mp4",
-            type: "video",
-          },
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-wind/projectWindExperience.png",
-            type: "image",
-          },
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-wind/projectWindProjects.png",
-            type: "image",
-          },
-        ],
-        mediaAlt: "",
-      },
-      {
-        name: "QUARANTINE APP",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/quarantine/quarantineVideo.mp4",
-            type: "video",
-          },
-        ],
-        mediaAlt: "",
-      },
-      {
-        name: "PROJECT MOON",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-moon/projectMoonPlacesVisited.mp4",
-            type: "video",
-          },
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-moon/projectMoonMain.png",
-            type: "image",
-          },
+    const assetBucketProjectUrlPrefix = `${AssetBucketUrlPrefix}/projects`;
+    const text = {
+        title: "PROJECTS",
+        contentItems: [
+            {
+                name: "PROJECT WIND",
+                date: "AUG - SEP 2019",
+                description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+                mediaList: [
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindMain`,
+                        type: "video",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindAbout`,
+                        type: "video",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindExperience`,
+                        type: "image",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindProjects`,
+                        type: "image",
+                    },
+                ],
+                mediaAlt: "",
+            },
+            // {
+            //     name: "QUARANTINE APP",
+            //     date: "AUG - SEP 2019",
+            //     description:
+            //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+            //     mediaList: [
+            //         {
+            //             src: `${assetBucketProjectUrlPrefix}/quarantine/quarantineVideo`,
+            //             type: "video",
+            //         },
+            //     ],
+            //     mediaAlt: "",
+            // },
+            {
+                name: "PROJECT MOON",
+                date: "AUG - SEP 2019",
+                description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+                mediaList: [
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonPlacesVisited`,
+                        type: "video",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonMain`,
+                        type: "image",
+                    },
 
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/project-moon/projectMoonStats.png",
-            type: "image",
-          },
-        ],
-        mediaAlt: "",
-      },
-      {
-        name: "STORYBOARD APP",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/story-board/storyboardFrameDark.png",
-            type: "image",
-          },
-        ],
-        mediaAlt: "",
-      },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonStats`,
+                        type: "image",
+                    },
+                ],
+                mediaAlt: "",
+            },
+            {
+                name: "STORYBOARD APP",
+                date: "AUG - SEP 2019",
+                description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+                mediaList: [
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardMain`,
+                        type: "video",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardPost`,
+                        type: "video",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebasePost`,
+                        type: "image",
+                    },
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebaseComments`,
+                        type: "image",
+                    },
+                ],
+                mediaAlt: "",
+            },
 
-      {
-        name: "PEDESTRIAN DETECTION SYSTEM",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/pedestrian-detection/pedestrianDetection.png",
-            type: "image",
-          },
+            {
+                name: "PEDESTRIAN DETECTION SYSTEM",
+                date: "AUG - SEP 2019",
+                description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+                mediaList: [
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/pedestrian-detection/pedestrianDetection`,
+                        type: "image",
+                    },
+                ],
+                mediaAlt: "",
+            },
+            {
+                name: "DROWSINESS DETECTION SYSTEM",
+                date: "AUG - SEP 2019",
+                description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+                mediaList: [
+                    {
+                        src: `${assetBucketProjectUrlPrefix}/drowsiness-detection/drowsinessDetection`,
+                        type: "video",
+                    },
+                ],
+                mediaAlt: "",
+            },
         ],
-        mediaAlt: "",
-      },
-      {
-        name: "DROWSINESS DETECTION SYSTEM",
-        date: "AUG - SEP 2019",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-        mediaList: [
-          {
-            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/projects/drowsiness-detection/DrowsinessDetectionSystem.mp4",
-            type: "video",
-          },
-        ],
-        mediaAlt: "",
-      },
-    ],
-  };
+    };
 
-  const textContainerVariants = {
-    initial: {},
-    animate: {
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.6,
-      },
-    },
-  };
-  const descriptionAnimationVariants = {
-    initial: {
-      y: 10,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        ease: "easeOut",
-        duration: 0.5,
-      },
-    },
-  };
+    const textContainerVariants = {
+        initial: {},
+        animate: {
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.6,
+            },
+        },
+    };
+    const descriptionAnimationVariants = {
+        initial: {
+            y: 10,
+            opacity: 0,
+        },
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                ease: "easeOut",
+                duration: 0.5,
+            },
+        },
+    };
 
-  return (
-    <>
-      <ProjectsHeader />
+    return (
+        <>
+            <ProjectsHeader />
 
-      <S.Container>
-        {text.contentItems.map((p, idx) => {
-          return (
-            <>
-              <S.ContainerItem idx={idx}>
-                <Carousel mediaList={p.mediaList} />
-                <S.TextItem
-                  idx={idx}
-                  variants={textContainerVariants}
-                  initial="initial"
-                  whileInView="animate"
-                >
-                  <motion.div variants={descriptionAnimationVariants}>
-                    <S.Title>{p.name}</S.Title>
-                    <S.Date>{p.date}</S.Date>
-                  </motion.div>
-                  <S.Description variants={descriptionAnimationVariants}>
-                    {p.description}
-                  </S.Description>
-                </S.TextItem>
-              </S.ContainerItem>
-            </>
-          );
-        })}
-      </S.Container>
-    </>
-  );
+            <S.Container>
+                {text.contentItems.map((p, idx) => {
+                    return (
+                        <>
+                            <S.ContainerItem idx={idx}>
+                                <Carousel mediaList={p.mediaList} />
+                                <S.TextItem
+                                    idx={idx}
+                                    variants={textContainerVariants}
+                                    initial="initial"
+                                    whileInView="animate"
+                                >
+                                    <motion.div
+                                        variants={descriptionAnimationVariants}
+                                    >
+                                        <S.Title>{p.name}</S.Title>
+                                        <S.Date>{p.date}</S.Date>
+                                    </motion.div>
+                                    <S.Description
+                                        variants={descriptionAnimationVariants}
+                                    >
+                                        {p.description}
+                                    </S.Description>
+                                </S.TextItem>
+                            </S.ContainerItem>
+                        </>
+                    );
+                })}
+            </S.Container>
+        </>
+    );
 };
