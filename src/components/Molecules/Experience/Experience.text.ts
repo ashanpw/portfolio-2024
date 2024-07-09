8c4cb24bcf92db5d79da4fadea7c298b90eea30a
Panduwawala <apanduwa@amazon.com>
2024-07-09 16:20:01 -0400

    feat: add husky pre-commit hook to lint code on commit

--- src/components/Molecules/Experience/Experience.text.ts
+++ src/components/Molecules/Experience/Experience.text.ts
@@ -1,39 +1,36 @@
 export const ExperienceText = {
-  title: "EXPERIENCE",
+  title: 'EXPERIENCE',
   index: 2,
   contentItems: [
     {
-      company: "AMAZON",
-      title: "SOFTWARE DEV ENGINEER II",
-      org: "PEOPLE ENGINE",
-      date: "NOV 2023 - PRESENT",
+      company: 'AMAZON',
+      title: 'SOFTWARE DEV ENGINEER II',
+      org: 'PEOPLE ENGINE',
+      date: 'NOV 2023 - PRESENT',
       description:
-        "Currently building a system to routinely check for missing Employee information such as Emergency contact info and routinely notify up to 3 million employees to update their data.",
-      imgSrc:
-        "https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/ess.png",
-      imgAlt: "",
+        'Built a service to routinely check for missing Employee information such as Emergency contact info and notify up to 3 million employees to update their data.',
+      imgSrc: 'https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/ess.png',
+      imgAlt: '',
     },
     {
-      company: "AMAZON",
-      title: "SOFTWARE DEV ENGINEER I",
-      org: "AMAZON GAME STUDIOS",
-      date: "DEC 2021 - NOV 2023",
+      company: 'AMAZON',
+      title: 'SOFTWARE DEV ENGINEER II',
+      org: 'AMAZON GAME STUDIOS',
+      date: 'DEC 2021 - NOV 2023',
       description:
-        "Part of the launch team for a new way of selling games on Amazon.com. The stack included a React / TypeScript frontend, and a GraphQL / Kotlin backend. Built the checkout system which handles purchasing and retrieving customer billing information.",
-      imgSrc:
-        "https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/amazonGameStudios.png",
-      imgAlt: "",
+        'Part of the launch team for a new way of selling games on Amazon.com. The stack included a React / TypeScript frontend, and a GraphQL / Kotlin backend. Built the checkout system which handles purchasing and retrieving customer billing information.',
+      imgSrc: 'https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/amazonGameStudios.png',
+      imgAlt: '',
     },
     {
-      company: "AWS",
-      title: "SOFTWARE DEV ENGINEER I",
-      org: "AWS APP MESH",
-      date: "FEB 2021 - DEC 2021",
+      company: 'AWS',
+      title: 'SOFTWARE DEV ENGINEER I',
+      org: 'AWS APP MESH',
+      date: 'FEB 2021 - DEC 2021',
       description:
-        "Engaged in on-call responsibilities, feature development, and contributed to regional infrastructure build-ups. Implemented a customer facing feature which allowed an Envoy proxy to intercept and manage DNS queries made by an application.",
-      imgSrc:
-        "https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/appMesh.png",
-      imgAlt: "",
+        'Engaged in on-call responsibilities, feature development, and contributed to regional infrastructure build-ups. Implemented a customer facing feature which allowed an Envoy proxy to intercept and manage DNS queries made by an application.',
+      imgSrc: 'https://ashanpw-asset-bucket.s3.amazonaws.com/experience-assets/appMesh.png',
+      imgAlt: '',
     },
   ],
 };
--- src/components/Molecules/Projects/Projects.tsx
+++ src/components/Molecules/Projects/Projects.tsx
@@ -1,191 +1,180 @@
-import { motion } from "framer-motion";
-import { IndentedContainer } from "../../Atoms/IndentedContainer/IndentedContainer";
-import { Carousel } from "../Carousel/Carousel";
-import { S } from "./Projects.styles";
-import {
-    AssetBucketUrlPrefix,
-    LineAnimationVariants,
-    ListAnimationVariants,
-} from "../../../utils/Constants";
-import { ProjectsHeader } from "../ProjectsHeader/ProjectsHeader";
+import { motion } from 'framer-motion';
+import { IndentedContainer } from '../../Atoms/IndentedContainer/IndentedContainer';
+import { Carousel } from '../Carousel/Carousel';
+import { S } from './Projects.styles';
+import { AssetBucketUrlPrefix, LineAnimationVariants, ListAnimationVariants } from '../../../utils/Constants';
+import { ProjectsHeader } from '../ProjectsHeader/ProjectsHeader';
 
 export const Projects = () => {
-    const assetBucketProjectUrlPrefix = `${AssetBucketUrlPrefix}/projects`;
-    const text = {
-        title: "PROJECTS",
-        contentItems: [
-            {
-                name: "PROJECT WIND",
-                date: "AUG - SEP 2019",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-                mediaList: [
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindMain`,
-                        type: "video",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindAbout`,
-                        type: "video",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindExperience`,
-                        type: "image",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindProjects`,
-                        type: "image",
-                    },
-                ],
-                mediaAlt: "",
-            },
-            // {
-            //     name: "QUARANTINE APP",
-            //     date: "AUG - SEP 2019",
-            //     description:
-            //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-            //     mediaList: [
-            //         {
-            //             src: `${assetBucketProjectUrlPrefix}/quarantine/quarantineVideo`,
-            //             type: "video",
-            //         },
-            //     ],
-            //     mediaAlt: "",
-            // },
-            {
-                name: "PROJECT MOON",
-                date: "AUG - SEP 2019",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-                mediaList: [
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonPlacesVisited`,
-                        type: "video",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonMain`,
-                        type: "image",
-                    },
-
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonStats`,
-                        type: "image",
-                    },
-                ],
-                mediaAlt: "",
-            },
-            {
-                name: "STORYBOARD APP",
-                date: "AUG - SEP 2019",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-                mediaList: [
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardMain`,
-                        type: "video",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardPost`,
-                        type: "video",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebasePost`,
-                        type: "image",
-                    },
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebaseComments`,
-                        type: "image",
-                    },
-                ],
-                mediaAlt: "",
-            },
-
-            {
-                name: "PEDESTRIAN DETECTION SYSTEM",
-                date: "AUG - SEP 2019",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-                mediaList: [
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/pedestrian-detection/pedestrianDetection`,
-                        type: "image",
-                    },
-                ],
-                mediaAlt: "",
-            },
-            {
-                name: "DROWSINESS DETECTION SYSTEM",
-                date: "AUG - SEP 2019",
-                description:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
-                mediaList: [
-                    {
-                        src: `${assetBucketProjectUrlPrefix}/drowsiness-detection/drowsinessDetection`,
-                        type: "video",
-                    },
-                ],
-                mediaAlt: "",
-            },
+  const assetBucketProjectUrlPrefix = `${AssetBucketUrlPrefix}/projects`;
+  const text = {
+    title: 'PROJECTS',
+    contentItems: [
+      {
+        name: 'PROJECT WIND',
+        date: 'AUG - SEP 2019',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!',
+        mediaList: [
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindMain`,
+            type: 'video',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindAbout`,
+            type: 'video',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindExperience`,
+            type: 'image',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-wind/projectWindProjects`,
+            type: 'image',
+          },
         ],
-    };
+        mediaAlt: '',
+      },
+      // {
+      //     name: "QUARANTINE APP",
+      //     date: "AUG - SEP 2019",
+      //     description:
+      //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!",
+      //     mediaList: [
+      //         {
+      //             src: `${assetBucketProjectUrlPrefix}/quarantine/quarantineVideo`,
+      //             type: "video",
+      //         },
+      //     ],
+      //     mediaAlt: "",
+      // },
+      {
+        name: 'PROJECT MOON',
+        date: 'AUG - SEP 2019',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!',
+        mediaList: [
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonPlacesVisited`,
+            type: 'video',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonMain`,
+            type: 'image',
+          },
 
-    const textContainerVariants = {
-        initial: {},
-        animate: {
-            transition: {
-                when: "beforeChildren",
-                staggerChildren: 0.6,
-            },
-        },
-    };
-    const descriptionAnimationVariants = {
-        initial: {
-            y: 10,
-            opacity: 0,
-        },
-        animate: {
-            y: 0,
-            opacity: 1,
-            transition: {
-                ease: "easeOut",
-                duration: 0.5,
-            },
-        },
-    };
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonStats`,
+            type: 'image',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/project-moon/projectMoonAbout`,
+            type: 'image',
+          },
+        ],
+        mediaAlt: '',
+      },
+      {
+        name: 'STORYBOARD APP',
+        date: 'AUG - SEP 2019',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!',
+        mediaList: [
+          {
+            src: `${assetBucketProjectUrlPrefix}/story-board/storyboardMain`,
+            type: 'video',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/story-board/storyboardPost`,
+            type: 'video',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebasePost`,
+            type: 'image',
+          },
+          {
+            src: `${assetBucketProjectUrlPrefix}/story-board/storyboardFirebaseComments`,
+            type: 'image',
+          },
+        ],
+        mediaAlt: '',
+      },
 
-    return (
-        <>
-            <ProjectsHeader />
+      {
+        name: 'PEDESTRIAN DETECTION SYSTEM',
+        date: 'AUG - SEP 2019',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!',
+        mediaList: [
+          {
+            src: `${assetBucketProjectUrlPrefix}/pedestrian-detection/pedestrianDetection`,
+            type: 'image',
+          },
+        ],
+        mediaAlt: '',
+      },
+      {
+        name: 'DROWSINESS DETECTION SYSTEM',
+        date: 'AUG - SEP 2019',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magnam assumenda est quos neque accusantium, eveniet incidunt earum beatae repudiandae dolor, labore voluptatem ducimus blanditiis vero enim libero unde dolorem!',
+        mediaList: [
+          {
+            src: `${assetBucketProjectUrlPrefix}/drowsiness-detection/drowsinessDetection`,
+            type: 'video',
+          },
+        ],
+        mediaAlt: '',
+      },
+    ],
+  };
 
-            <S.Container>
-                {text.contentItems.map((p, idx) => {
-                    return (
-                        <>
-                            <S.ContainerItem idx={idx}>
-                                <Carousel mediaList={p.mediaList} />
-                                <S.TextItem
-                                    idx={idx}
-                                    variants={textContainerVariants}
-                                    initial="initial"
-                                    whileInView="animate"
-                                >
-                                    <motion.div
-                                        variants={descriptionAnimationVariants}
-                                    >
-                                        <S.Title>{p.name}</S.Title>
-                                        <S.Date>{p.date}</S.Date>
-                                    </motion.div>
-                                    <S.Description
-                                        variants={descriptionAnimationVariants}
-                                    >
-                                        {p.description}
-                                    </S.Description>
-                                </S.TextItem>
-                            </S.ContainerItem>
-                        </>
-                    );
-                })}
-            </S.Container>
-        </>
-    );
+  const textContainerVariants = {
+    initial: {},
+    animate: {
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.6,
+      },
+    },
+  };
+  const descriptionAnimationVariants = {
+    initial: {
+      y: 10,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        ease: 'easeOut',
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <>
+      <ProjectsHeader />
+
+      <S.Container>
+        {text.contentItems.map((p, idx) => {
+          return (
+            <>
+              <S.ContainerItem idx={idx}>
+                <Carousel mediaList={p.mediaList} />
+                <S.TextItem idx={idx} variants={textContainerVariants} initial="initial" whileInView="animate">
+                  <motion.div variants={descriptionAnimationVariants}>
+                    <S.Title>{p.name}</S.Title>
+                    <S.Date>{p.date}</S.Date>
+                  </motion.div>
+                  <S.Description variants={descriptionAnimationVariants}>{p.description}</S.Description>
+                </S.TextItem>
+              </S.ContainerItem>
+            </>
+          );
+        })}
+      </S.Container>
+    </>
+  );
 };
