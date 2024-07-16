6c9edc84286e448356b14f3ca3b3b19932a4fa30
ashanpw <ashan399@gmail.com>
2024-07-15 22:46:16 -0700

    feat: adjust animation pacing in Lines of Code component

--- src/components/Molecules/LinesOfCode/LinesOfCode.tsx
+++ src/components/Molecules/LinesOfCode/LinesOfCode.tsx
@@ -1,219 +1,122 @@
-import { useRef } from "react";
-import { S } from "./LinesOfCode.styles";
-import {
-  motion,
-  useMotionValueEvent,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-import { AssetBucketUrlPrefix } from "../../../utils/Constants";
+import { useRef } from 'react';
+import { S } from './LinesOfCode.styles';
+import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion';
+import { AssetBucketUrlPrefix } from '../../../utils/Constants';
 
 const randomIntFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 export const LinesOfCode = () => {
   const text = {
-    title: "100K+",
-    subtitle: "LINES OF CODE",
+    title: '100K+',
+    subtitle: 'LINES OF CODE',
   };
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: [-0.8, 1, 2, 2],
+    offset: [-0.8, 1, 1.5, 2],
     smooth: 5,
   });
 
   const imageList = [
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/drowsinessDetection.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/drowsinessDetection.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonPlacesVisited.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonPlacesVisited.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/characterRecognition.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/characterRecognition.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/depthDetection.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/depthDetection.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/jumpBoxGame.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/jumpBoxGame.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/panoramaStitching.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/panoramaStitching.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/pedestrianDetection.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/pedestrianDetection.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonHome.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonHome.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/unityARProject.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonOld.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindExperience.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindExperience.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindHome.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindHome.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDeadIsland2.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDeadIsland2.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/cannyLandscape.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/cannyLandscape.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/primeGaming.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonOldExperience.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/jumpmanGame.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectMoonHome.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-400, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/seattleHouseListingCongestion.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/seattleHouseListingCongestion.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/bruteForce.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/bruteForce.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
 
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/turboCheckout.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/turboCheckout.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/studyBubble.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/studyBubble.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDetailPage2.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDetailPage2.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/storyboardAppLoginScreen.PNG`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/storyboardAppFrame.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDetailPage1.png`,
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/ggDetailPage1.webp`,
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
 
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/quarantinePoster.png`,
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/quarantinePoster.webp`,
       y: useTransform(scrollYProgress, [1, 0], [-3000, 200]),
     },
     {
-      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindMain.png`,
+      imgSrc: `${AssetBucketUrlPrefix}/lines-of-code-images/projectWindMain.webp`,
       x: randomIntFromInterval(-200, 2),
-      y: useTransform(
-        scrollYProgress,
-        [1, 0],
-        [randomIntFromInterval(-800, 400), 0]
-      ),
+      y: useTransform(scrollYProgress, [1, 0], [randomIntFromInterval(-800, 400), 0]),
     },
   ];
 
